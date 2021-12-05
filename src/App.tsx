fba8798862ce9049fd6d2f0bed2bfe8fc73b3d5a
chitteshwari <80682927+chitteshwari@users.noreply.github.com>
2021-12-05 21:05:45 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,45 @@
-import React, { FC, ChangeEvent, useState } from "react";
-import "./App.css";
-import TodoTask from "./Components/TodoTask";
-import { ITask } from "./Interfaces";
+import React, {FC, ChangeEvent,useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ITask} from './Interfaces'
+import TodoTask from './Components/TodoTask';
 
-const App: FC = () => {
+const App: FC = () => { 
+
   const [task, setTask] = useState<string>("");
-  const [deadline, setDealine] = useState<number>(0);
+  const [deadline, setDeadline] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {
-      setTask(event.target.value);
-    } else {
-      setDealine(Number(event.target.value));
+      setTask(event.target.value)
+    }
+    else {
+      setDeadline(Number(event.target.value))
     }
   };
 
-  const addTask = (): void => {
-    const newTask = { taskName: task, deadline: deadline };
-    setTodoList([...todoList, newTask]);
-    setTask("");
-    setDealine(0);
-  };
+const addTask = (): void => {
+  const newTask = {taskName: task, deadline: deadline};
+  setTodoList([...todoList, newTask]);
+  console.log(todoList);
+  setTask("");
+  setDeadline(0);
+}
 
-  const completeTask = (taskNameToDelete: string): void => {
-    setTodoList(
-      todoList.filter((task) => {
-        return task.taskName != taskNameToDelete;
-      })
-    );
-  };
+const completeTask = (taskNameToDelete: string): void => {
+  setTodoList(todoList.filter((task) => {
+    return task.taskName != taskNameToDelete 
+  } ))
 
-  return (
+};
+
+  return  (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input
-            type="text"
-            placeholder="Task..."
-            name="task"
-            value={task}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            placeholder="Deadline (in Days)..."
-            name="deadline"
-            value={deadline}
-            onChange={handleChange}
-          />
+          <input type="text" placeholder="Task..." name="task" value={task} onChange={handleChange} />
+          <input type="number" placeholder="Deadline (in Days)..." name="deadline" value={deadline} onChange={handleChange} />
         </div>
         <button onClick={addTask}>Add Task</button>
       </div>
